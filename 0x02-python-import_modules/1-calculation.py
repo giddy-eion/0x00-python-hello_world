d098fb34ff426c82d733844ab634fc946206546f
giddy-eion <gideonosamudiame@gmail.com>
2023-01-30 19:45:24 +0000

    added files

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this program perform basic calculator functions"""
+
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    """function add: adds (a + b)
+
+    sub: subtracts (a, b)
+
+    mul: multiplies (a, b)
+
+    div: divides (a, b)
+    """
+
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:.0f}".format(a, b, div(a, b)))
